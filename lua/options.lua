2b28c19cffb250c2916c1d6afa9348c32c364927
Ryan <joshua.r.langer@gmail.com>
2024-06-01 11:43:32 -0400

    Separated out kickstart config into separate files - added additional options and keymaps

---
+++ lua/options.lua
@@ -0,0 +1,29 @@
+vim.opt.number = true
+vim.opt.mouse = 'a'
+vim.opt.showmode = false
+vim.opt.breakindent = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.signcolumn = 'yes'
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.inccommand = 'split'
+vim.opt.cursorline = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv 'userprofile' .. '/AppData/Local/nvimundo/'
+vim.opt.undofile = true
+vim.opt.isfname:append '@-@'
+vim.opt.updatetime = 50
+vim.opt.colorcolumn = '120'
+vim.opt.scrolloff = 10
