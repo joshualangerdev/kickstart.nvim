22a3042c1a5a2ba4d577ee845751f9be3d8c71c7
Joshua Langer <111317248+joshualangerdev@users.noreply.github.com>
2024-07-13 14:58:19 -0400

    LSP Updates
    Turned off rust and omnisharp. Added cmake, make and markdown lsp

--- lua/kickstart/plugins/lsp.lua
+++ lua/kickstart/plugins/lsp.lua
@@ -159,8 +159,11 @@ return {
         clangd = {},
         -- gopls = {},
         -- pyright = {},
-        rust_analyzer = {},
-        omnisharp_mono = {},
+        --  rust_analyzer = {},
+        -- omnisharp_mono = {},
+        cmake = {},
+        autotools_ls = {},
+        zk = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
